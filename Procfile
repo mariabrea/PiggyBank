87ca51726fd286c1ee6d1de71eb062bad35b26eb
mariabrea <mariabrea75@hotmail.com>
2023-03-01 09:12:31 -0600

    Added .gitignore

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: node piggybank.js
--- piggybank.js
+++ piggybank.js
@@ -177,6 +177,6 @@ app.get("/graph", function(req, res){
     
 });
 
-app.listen(3000, function(){
+app.listen(process.env.PORT || 3000, function(){
     console.log("Server started on port 3000.")
 });
